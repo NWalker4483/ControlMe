4250c191506aabc2585721aa066072ef26312075
Nile Walker <niwal7@morgan.edu>
2018-02-21 22:30:20 -0500

    MInor fixes to pull push

--- Pull_Push.py
+++ Pull_Push.py
@@ -1,14 +1,14 @@
 
 import serial
 class Linear_Actuator():
-    def __init__(path="/dev/ttyACM0"):
+    def __init__(self,path="/dev/ttyACM0"):
         self.ratio=4095/100
         self.path=serial.Serial(port=path) 
     def move(self,goal):
-        self.path.write(("#"+int(goal*self.ratio).encode('utf-8'))
+        self.path.write(("#"+str(int(goal*self.ratio)).encode('utf-8')))
 
 if __name__=="__main__":
-test=Linear_Actuator('/dev/ttyACM0')
+    test=Linear_Actuator()
     while True:
         test.move(int(input()))
     test.path.close()
