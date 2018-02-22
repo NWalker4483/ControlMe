8eb8c4dea21a277ba93119c70dea1f19f1dd6340
Nile Walker <niwal7@morgan.edu>
2018-02-21 22:26:43 -0500

    Nothing relevant comments removed

--- Pull_Push.py
+++ Pull_Push.py
@@ -1,12 +1,4 @@
-/*
-* Motor control setup for pololu jrk21v3 with Arduino UNO R3, verified using linear actualtor LACT2P
-*
-* Pololu jrk config utility in Serial mode using UART detect baud rate interface. 
-* starting with the default configuration settings for LACT2P linear actuators provided on the pololu website
-*
-* pin 8 connected to jrk pin Rx
-* jrk grnd connected to arduino ground
-*/
+
 import serial
 class Linear_Actuator():
     def __init__(path="/dev/ttyACM0"):
